35e3c8ca67c579c4a849f05b58f9721c2161c5f1
Asim Esa <intelligentzaram@gmail.com>
2023-01-28 17:48:25 +0400

    add some Style via MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,69 @@
 import React from "react";
-
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+} from "@mui/material";
+import { PhotoCamera } from "@mui/icons-material";
 
 const App = () => {
-    return (
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
         <div>
-            <h1>Hello World!</h1>
+          <Container maxWidth="sm" style={{ marginTop: "100px" }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello Evryone This is a photo album and I'm Trying to make this
+              sentence as long as possible so we can see how does it look like
+              on the screen
+            </Typography>
+            <div>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
         </div>
-    )
-}
-
+      </main>
+    </>
+  );
+};
 
 export default App;
