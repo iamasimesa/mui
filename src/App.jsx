469c56cf3eecd88de6da01a05799b8496dd36e1b
Asim Esa <intelligentzaram@gmail.com>
2023-01-29 16:13:37 +0400

    change mui with styles but not working

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,17 @@ import {
   Button,
 } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
+// import { makeStyles } from '@mui/styles';
+
+// const useStyles = makeStyles((theme) => ({
+// container: {
+//   backgroundColor: theme.palette.background.paper,
+//   padding: theme.spacing(8, 0, 6)
+// }
+// }));
 
 const App = () => {
+  // const classes = useStyles();
   return (
     <>
       <CssBaseline />
